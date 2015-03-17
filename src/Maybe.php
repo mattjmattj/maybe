2bd635543fd042d88bd4f2fa4116bc75f25080ba
Matthias Jouan <matthias.jouan@gmail.com>
2015-03-17 21:56:13 +0000

    Scenario #1 : Maybe can wrap an object without altering it

--- src/Maybe.php
+++ src/Maybe.php
@@ -4,4 +4,18 @@ namespace Maybe;
 
 class Maybe {
 	
+	/**
+	 * @param string $classname - the name of the class/interface we want to wrap
+	 */ 
+	public function __construct ($classname) {
+		
+	}
+	
+	/**
+	 * @param mixed $object - the object we want to wrap. May be null
+	 */ 
+	public function wrap ($object) {
+		return $object;
+	}
+	
 }
--- tests/MaybeTest.php
+++ tests/MaybeTest.php
@@ -2,8 +2,35 @@
 
 namespace Maybe\Tests;
 
+use Maybe\Maybe;
+
 class MaybeTest extends \PHPUnit_Framework_TestCase {
 	
+	public function testMaybeCanWrapAnObjectWithoutAlteringIt () {
+		
+		$maybe = new Maybe('Maybe\Tests\Simple');
+		$simple = new Simple();
+		
+		$wrapped = $maybe->wrap($simple);
+		
+		$this->assertEquals($simple->doSomething(), $wrapped->doSomething());
+		$this->assertEquals($simple->doSomethingElse(), $wrapped->doSomethingElse());
+	}
+}
+
+
+class Simple {
+	/**
+	 * @return int
+	 */ 
+	public function doSomething () {
+		return 3;
+	}
 	
-	
+	/**
+	 * @return string
+	 */ 
+	public function doSomethingElse () {
+		return 'foobar';
+	}
 }
